659f88b09bc1590ca53a54057cfd35fff8e2f8c6
Siddhii9 <sr23csb0f08@student.nitw.ac.in>
2025-06-06 11:03:19 +0530

    added missing react entry files

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
@@ -8,12 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('CampusKart backend is running!');
+app.get("/", (req, res) => {
+  res.send("CampusKart backend is running!");
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
